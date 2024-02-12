1129ef1019d61d019705135a38b8b6778e6e28f8
unknown <fernando.zigarra@gmail.com>
2024-02-12 15:41:08 -0300

    add dinamic routing

---
+++ src/routes/index.ts
@@ -0,0 +1,23 @@
+import { Router } from "express";
+import { readdirSync } from 'fs'
+
+const PATH_ROUTER = `${__dirname}`
+
+const router = Router()
+
+
+const clearFileName = (filename: string) => {
+    const file = filename.split('.').shift()
+    return file
+}
+readdirSync(PATH_ROUTER).filter((filename) => {
+    const cleanName = clearFileName(filename);
+    if (cleanName !== "index") {
+        import(`./${cleanName}`).then((moduleRouter) => {
+            console.log(`File import and loaded: ${cleanName}`)
+            router.use(`/${cleanName}`, moduleRouter.router)
+        })
+    }
+})
+
+export { router }
