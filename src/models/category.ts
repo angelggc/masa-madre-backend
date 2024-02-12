a16da876f4fd3b743896f4feb6d7e675e945deb9
angel <angelgimenez992@gmail.com>
2024-02-12 16:37:06 -0300

    add models and entities

---
+++ src/models/category.ts
@@ -0,0 +1,10 @@
+import { Schema, model } from "mongoose";
+import { ICategory } from "../entities/category";
+
+const categorySchema = new Schema({
+  productType: { type: String, required: true },
+  description: { type: String, required: true },
+  products: { type: String, required: true },
+});
+
+const Category = model<ICategory>("Category", categorySchema);
---
+++ src/models/product.ts
@@ -0,0 +1,12 @@
+import { Schema, model } from "mongoose";
+import { IProduct } from "../entities/product";
+
+const productSchema = new Schema({
+  name: { type: String, required: true },
+  ingredients: [{ type: String, required: true }],
+  image: { type: String, required: true },
+  description: { type: String },
+  price: { type: Number, required: true },
+});
+
+const Product = model<IProduct>("Product", productSchema);
