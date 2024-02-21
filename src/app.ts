d2d90f7361723b5290f5485ceb0dcb0050d235a1
Damian Fernandez <90562495+damianfe@users.noreply.github.com>
2024-02-21 11:42:50 -0300

    feat: add model and controller user done

--- src/app.ts
+++ src/app.ts
@@ -4,6 +4,7 @@ import cors from "cors";
 import { dbConnect } from "./config/db";
 import categoryRoutes from "./routes/category";
 import productRoutes from "./routes/product";
+import userRoutes from "./routes/user"
 const PORT = process.env.PORT || 3050;
 
 export const app = express();
@@ -12,6 +13,7 @@ app.use(express.json());
 dbConnect();
 app.use("/api/v1", categoryRoutes);
 app.use("/api/v1", productRoutes);
+app.use("/api/v1", userRoutes);
 app.listen(PORT, () => {
   console.log(`Server is running in ${PORT}`);
 });
---
+++ src/middlewares/index.ts
@@ -0,0 +1,42 @@
+import { NextFunction, Request, Response } from "express";
+import jwt from "jsonwebtoken";
+import { TokenInterface, errorResponse } from "../utils/index";
+import User from "../models/user";
+import createHttpError from "http-errors";
+declare global {
+    namespace Express {
+      interface Request {
+        user?: any; // Puedes ajustar el tipo según la estructura de tu usuario
+      }
+    }
+  }
+
+  
+export const checkAuth = async (req: Request, res: Response, next: NextFunction) => {
+
+    let token;
+
+    if(req.headers.authorization && req.headers.authorization.startsWith("Bearer")){
+        try {
+
+            token = req.headers.authorization.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET as string);
+
+            req.user = await User.findById((decoded as TokenInterface).id).select(
+                    "-password -checked -token -createdAt -updatedAt -__v"
+                  );
+            if(!req.user) throw createHttpError(401, "Usuario inválido")
+                
+        } catch (error) {
+            return errorResponse(res, error, "VERIFY-JWT")
+        }
+    }
+
+    if(!token) {
+        const error = createHttpError(401, "Token inválido")
+        return errorResponse(res, error, "VERIFY-JWT")
+ 
+    }
+
+    next()
+}
